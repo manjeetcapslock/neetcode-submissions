edde3d739686f189c664e8df9b5969ef50dc2930
manjeetcapslock <manjeet.swain@gmail.com>
2026-07-25 12:23:52 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict={}
+        for s in strs:
+            key=''.join(sorted(s))
+            if key not in dict:
+                dict[key]=[]
+            dict[key].append(s)
+        return list(dict.values())
+      
+
+
+                
+        
